bce63641c7dc080bc5652c0b5548a43c7c530410
nuesslerm <markus@choco.com>
2020-05-11 01:21:18 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      meaningOfLife: 42,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ meaningOfLife: this.state.meaningOfLife + 1 });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.meaningOfLife}</p>
+          <button onClick={this.handleClick}>Update State</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
